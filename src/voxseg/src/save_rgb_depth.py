80cffd9f4a396996eecd9048c2cd01ded7593285
Sean Brynjolfsson <smb459@cornell.edu>
2023-07-31 09:17:55 -0400

    Implement callback in save_rgb_depth.py for extrinsics and publishing the entire rgbd.

--- src/voxseg/src/save_rgb_depth.py
+++ src/voxseg/src/save_rgb_depth.py
@@ -5,6 +5,7 @@ from sensor_msgs.msg import CompressedImage, Image
 from message_filters import ApproximateTimeSynchronizer, Subscriber
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
+import numpy as np
 
 class ImageSaver:
     def __init__(self):
@@ -22,13 +23,18 @@ class ImageSaver:
         try:
             # Convert RGB compressed image to OpenCV format
             rgb_img = self.bridge.compressed_imgmsg_to_cv2(rgb_msg, desired_encoding="bgr8")
+            #cv2.imwrite(f'rgb_{rgb_msg.header.stamp}.jpg', rgb_img)
+            rgb_img_np = np.array(rgb_img)
             
             # Convert depth image to OpenCV format
-            depth_img = self.bridge.imgmsg_to_cv2(depth_msg, desired_encoding="32FC1")
+            depth_img = self.bridge.imgmsg_to_cv2(depth_msg, desired_encoding="passthrough")
+            #cv2.imwrite(f'depth_{depth_msg.header.stamp}.png', depth_img)
+            depth_img_np = np.array(depth_img)
+
+            # TODO: Make this work!!!
+            # self.data.add_depth_image(np_image, np_depths, extrinsics_2d)
+            # print(f'Depth Image {len(self.data.all_images)} Received')
             
-            # Save the images using a filename with the timestamp
-            cv2.imwrite(f'rgb_{rgb_msg.header.stamp}.jpg', rgb_img)
-            cv2.imwrite(f'depth_{depth_msg.header.stamp}.png', depth_img)
             
             rospy.loginfo("Saved synchronized images with timestamp: %s", rgb_msg.header.stamp)
             
@@ -36,7 +42,8 @@ class ImageSaver:
             rospy.logerr(e)
 
 if __name__ == '__main__':
-    rospy.loginfo("Set up (rgb,depth) image saver node")
+
     rospy.init_node('image_saver_node')
+    rospy.loginfo("Set up (rgb,depth) image saver node")
     ImageSaver()
     rospy.spin()
