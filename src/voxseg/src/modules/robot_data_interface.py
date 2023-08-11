0d8b0cc07dc86a910b00215b01efa4f8e67a5073
Will H <willhu003@gmail.com>
2023-08-11 11:18:20 -0400

    update classes and saving online

--- src/voxseg/src/modules/robot_data_interface.py
+++ src/voxseg/src/modules/robot_data_interface.py
@@ -56,7 +56,6 @@ class OVTDataInterface:
         self.ovt_request_service = rospy.get_param('/ovt/REQUEST_SERVICE')
         self.batch_size = rospy.get_param('/ovt/BATCH_SIZE')
         self.change_rate_topic = rospy.get_param('/ovt/CHANGE_RATE_TOPIC')
-        self.change_buffer_size_topic = rospy.get_param('/ovt/CHANGE_BUFFER_SIZE_TOPIC')
 
 
         self.bridge = CvBridge()
@@ -65,28 +64,25 @@ class OVTDataInterface:
         self.rate = rospy.get_param('/ovt/COMPUTE_PERIOD')
         self.classes = list(rospy.get_param('/ovt/CLASSES'))
         self.base_name = rospy.get_param('/ovt/BASE_NAME')
+        self.use_large = rospy.get_param('/ovt/USE_LARGE')
 
-        self.encoder = WSImageEncoder(VOXSEG_ROOT_DIR, config='configs/ovt_small.yaml')
-        #self.encoder = WSImageEncoder(VOXSEG_ROOT_DIR, config='configs/ovt_small.yaml')
+        if self.use_large:
+            config='configs/ovt.yaml'
+        else:
+            config='configs/ovt_small.yaml'
+        self.encoder =  WSImageEncoder(root_dir=VOXSEG_ROOT_DIR, config=config, use_large=self.use_large)
+        
         self.device = rospy.get_param('ovt/DEVICE')
         
-        # self.buffer contains tuples of (RosImage, CameraInfo representing camera for that image)
-        self.buffer = TriggerBuffer(maxlen = self.batch_size, fn=self.request_computation,  clear_on_trigger=True)
-        self._lock_buffer = False
 
         rospy.init_node(self.data_interface_node)
 
         # User Input Subscriptions
         RospySubscriber(self.change_rate_topic, Float32, callback=self.change_rate)
-        RospySubscriber(self.change_buffer_size_topic, Int32, callback = self.change_buffer_size)
         RospySubscriber(self.class_topic, Classes, self.class_name_callback)
 
         # Elevation Mapping Plugin Publishers
-        prob_publisher_topics = list(rospy.get_param('/ovt/PROB_TOPICS'))
-        self.class_pub_register = {}
-        for i, topic in enumerate(prob_publisher_topics):
-            pub = Publisher(topic, RosImage, queue_size=10)
-            self.class_pub_register[self.classes[i]] = pub
+        self.define_pub_register()
 
         self.mask_pub = Publisher('/ovt/masked_image', RosImage, queue_size=10)
 
@@ -97,40 +93,16 @@ class OVTDataInterface:
         print('OVT Interface Initialized')
         self.time_request = time.time()
         rospy.spin()
-            
 
-    def request_computation(self, buffer):
-        """
-        This is bound to the trigger buffer, so it is a function on the buffer
-
-        Buffer contains tuples of images and extrinsics
-
-        Inputs:
-            images: numpy array of images, shape (B, 3, H, W)
-            
-            tfs: numpy array of tfs, shape (B, 4, 4)
-        """
-        print(f'Time since last request: {time.time()-self.time_request}')
-        self.time_request = time.time()
-        # with self.lock_buffer(buffer) as buffer_freeze:
-        #     all_probs_msg = self._handle_compute_request(buffer_freeze, self.classes)
-        #     self.publish_probs_and_tfs(all_probs_msg)
-        
-        
-
-    @contextmanager
-    def lock_buffer(self, buffer):
-
-        buffer_freeze = list(buffer).copy()
-        self._lock_buffer=True
-
-        try:
-            yield buffer_freeze
-        finally:
-            self._lock_buffer=False
-
+    def define_pub_register(self):
+        prob_publisher_topics = list(rospy.get_param('/ovt/PROB_TOPICS'))
+        self.class_pub_register = {}
+        for i, topic in enumerate(prob_publisher_topics):
+            pub = Publisher(topic, RosImage, queue_size=10)
+            self.class_pub_register[self.classes[i]] = pub
 
     def _handle_compute_request(self, images, classes):
+
         """
         assumes only 1 item in images
         """
@@ -138,30 +110,33 @@ class OVTDataInterface:
         
         # Update from the most recent tensors 
         images_msg = list(images)
-        
         classes = [str(c) for c in classes]
-        
         images = torch_from_img_array_msg(images_msg).float().to(self.device)
         t1  = rospy.get_time() # replace with torch events
-        class_probs = self.encoder.call_with_classes(images, classes, use_adapter=True)
-                
+        
+        # perform computation
+        class_probs = self.encoder.call_with_classes(images, classes, use_adapter=True)        
         classifications = torch.argmax(class_probs[0], dim=0)
         classifications = classifications / classifications.max()
+        
+        # get and publish masks
         masked_overlay, mask, image, cv2_overlay = get_turbo_image(images[0], classifications)
         mask_msg = bridge.cv2_to_imgmsg(cv2_overlay, header=images_msg[0].header)
         self.mask_pub.publish(mask_msg)
         print(f"CLIP Inference and ROS Serialization Time: {rospy.get_time() - t1}")
 
-        time = rospy.get_time()
-        base_path = os.path.join(VOXSEG_ROOT_DIR, 'output1')
-        if not os.path.exists(base_path):
-            os.mkdir(base_path)
-        masked_overlay.save(os.path.join(base_path, f'{self.base_name}_mask_{time}.jpg'))
-        image.save(os.path.join(base_path, f'{self.base_name}_image_{time}.jpg'))
-
-
-        all_probs_msg = []
+        # Save images
+        if rospy.get_param('/ovt/SAVE_IMAGES'):
+            base_path = os.path.join(VOXSEG_ROOT_DIR, 'output')
+            if not os.path.exists(base_path):
+                os.mkdir(base_path)
+            time = rospy.get_time()
         
+            masked_overlay.save(os.path.join(base_path, f'{self.base_name}_mask_{time}.jpg'))
+            image.save(os.path.join(base_path, f'{self.base_name}_image_{time}.jpg'))
+
+        # publish probabilities
+        all_probs_msg = []
         for i, multi_channel_probs in enumerate(class_probs):
             c, _, _ = multi_channel_probs.size()
 
@@ -209,31 +184,13 @@ class OVTDataInterface:
         all_probs_msg = self._handle_compute_request(image_list, self.classes)
         self.publish_probs_and_tfs(all_probs_msg)
     
-
     def class_name_callback(self, msg):
         classes = list(msg.classes)
+        self.classes=classes
+        self.define_pub_register()
         print(f'Classes recieved: {classes}')
 
     def change_rate(self, msg):
         new_rate = float(msg.data)
         self.rate = new_rate
-
-    def change_buffer_size(self, msg):
-        new_size = int(msg.data)
-
-        new_buffer = deque(maxlen=new_size)
-
-        for i in self.buffer:
-            new_buffer.append(i)
-        
-        self.buffer = new_buffer
-
-        if new_size < len(self.buffer):
-            rospy.logwarn('Warning: decreased buffer size, resulting in lost data')
-
-
-    def save_img(self, img, base='rgb'):
-
-        
-        cv2.imwrite(F"{VOXSEG_ROOT_DIR}/output/{base}_{self.tick // self.rate}.png", img)
 
